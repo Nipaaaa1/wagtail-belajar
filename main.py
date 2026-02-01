ca3fd4a6d7b64f8462f241a3a707f0dff3672138
Nipaaaa1 <frostone224@gmail.com>
2026-02-01 20:56:41 +0000

    chore: setup wagtail

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from wagtail-belajar!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-a*=+ps5i7&rx3#u(k%uswy!6jlwbb&)(n$xkxg=g%&nlcne^xa"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
