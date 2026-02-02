a6dbd82128b1fbebffa1137ff146c0f0b443ae2d
Nipaaaa1 <frostone224@gmail.com>
2026-02-02 08:19:27 +0000

    feat: add database models

---
+++ blog/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django import forms
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.models import ParentalKey, ParentalManyToManyField
+from taggit.models import TaggedItemBase
+from wagtail.fields import RichTextField
+from wagtail.models import Orderable, Page
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.snippets.models import register_snippet
+from datetime import date
+
+
+class BlogIndexPage(Page):
+    description = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("description")]
+
+
+class BlogPostTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "BlogPostPage", related_name="tagged_items", on_delete=models.CASCADE
+    )
+
+
+class BlogPostPage(Page):
+    date = models.DateField("Post Date", default=date.today)
+    intro = RichTextField(blank=True)
+    content = RichTextField(blank=True)
+    authors = ParentalManyToManyField("blog.Author", blank=True)
+    tags = ClusterTaggableManager(through=BlogPostTag, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
+        FieldPanel("date"),
+        FieldPanel("intro"),
+        FieldPanel("content"),
+        InlinePanel("image_gallery", label="Images Gallery"),
+        FieldPanel("tags"),
+    ]
+
+
+class BlogImageGallery(Orderable):
+    page = ParentalKey(
+        BlogPostPage, related_name="image_gallery", on_delete=models.CASCADE
+    )
+
+    image = models.ForeignKey(
+        "wagtailimages.image", related_name="+", on_delete=models.CASCADE
+    )
+
+    caption = models.CharField(max_length=255, blank=True)
+
+    panels = [FieldPanel("image"), FieldPanel("caption")]
+
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        "wagtailimages.image", related_name="+", on_delete=models.CASCADE
+    )
+
+    panels = [FieldPanel("name"), FieldPanel("author_image")]
+
+    def __str__(self):
+        return self.name
+
+
+class TagIndexPage(Page):
+    pass
